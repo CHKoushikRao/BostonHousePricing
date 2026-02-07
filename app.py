a07d1073e976232ba8d6df19e80d4cc5391f2e90
CHKoushikRao <chkoushikrao@gmail.com>
2026-02-07 21:14:17 +0530

    made the prediction using web with float and string type input

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model=pickle.load(open('house_price_model.pkl','rb'))
 @app.route ('/')
 def home():
     return render_template('home.html')
-
+"""
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
@@ -19,7 +19,26 @@ def predict_api():
     #print(data_array)
     prediction=model.predict(data_array)
     #print(prediction)
-    return jsonify(prediction[0])
+    return jsonify(prediction[0])"""
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = {
+        "longitude": float(request.form["longitude"]),
+        "latitude": float(request.form["latitude"]),
+        "housing_median_age": float(request.form["housing_median_age"]),
+        "total_rooms": float(request.form["total_rooms"]),
+        "total_bedrooms": float(request.form["total_bedrooms"]),
+        "population": float(request.form["population"]),
+        "households": float(request.form["households"]),
+        "median_income": float(request.form["median_income"]),
+        "ocean_proximity": request.form["ocean_proximity"]  # string stays string
+    }
+
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)[0]
+    return render_template('home.html',prediction_text="The predicted price of the house is {}".format(prediction))
+
 
 if __name__=="__main__":
     app.run(debug=True)
